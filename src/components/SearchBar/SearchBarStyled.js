4188b652a9f23d5278098e8d63128ce85446ab2d
Brian Cortright <brcortright@gmail.com>
2024-02-06 13:29:03 -0800

    bugfix: Search input bg and placeholder colors

--- src/components/SearchBar/SearchBarStyled.js
+++ src/components/SearchBar/SearchBarStyled.js
@@ -1,11 +1,12 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 import { baseColors, basePadding, fontSizing, transition } from "#/theme";
 
 export const SearchInput = styled.input`
+  background: ${baseColors.gray1};
   border: 1px solid ${baseColors.gray1};
   border-radius: 4px;
   color: ${baseColors.white};
-  font-family: 'Poppins', sans-serif;
+  font-family: "Poppins", sans-serif;
   font-size: ${fontSizing.base};
   margin: ${basePadding.medium} 0;
   max-width: 1200px;
@@ -13,6 +14,10 @@ export const SearchInput = styled.input`
   padding: ${basePadding.medium};
   transition: ${transition.default};
   width: 100%;
+
+  &::placeholder {
+    color: ${baseColors.white};
+  }
 
   &:focus {
     border-color: ${baseColors.primary};
